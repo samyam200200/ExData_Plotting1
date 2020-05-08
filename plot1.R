c6c085d44252bf5e3a45faa92e2b566eafda40b9
Samyam Aryal <59404015+samyam200200@users.noreply.github.com>
2020-05-08 07:29:15 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
+#Reads the file and cleans it. Selects two dates
 data<-read.table("household_power_consumption.txt", sep = ";",na.strings = "?", header=T, colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
 head(data)
 data$Date<-as.Date(data$Date, "%d/%m/%Y")
@@ -6,7 +7,7 @@ a<-na.omit(a)
 library(tidyr)
 b<-unite(a, DateTime,Date,Time, sep=" ")
 b$DateTime<-strptime(b$DateTime, "%Y-%m-%d %H:%M:%S")
-names(b)
+#Produces the first plot as png
 png("plot1.png", width = 480, height = 480)
 hist(a$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="Red")
 dev.off()
